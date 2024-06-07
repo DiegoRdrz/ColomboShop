8878ab2463a10766a0bcff7fb14327186abb4c09
DiegoRdrz <diegordrz455@gmail.com>
2024-06-07 15:25:57 -0500

    Creando proyecto

---
+++ Back-End/src/main/java/com/BackEnd/Colomboshop/ColomboshopApplication.java
@@ -0,0 +1,13 @@
+package com.BackEnd.Colomboshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColomboshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColomboshopApplication.class, args);
+	}
+
+}
